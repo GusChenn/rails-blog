8809ca18449faa420437f9f3d4379b514d6738ad
Gustavo Chen <gustavo.sao@gmail.com>
2024-11-30 21:41:08 -0300

    Finish roulette and add new whatever style

--- app/components/main_page/icon_roulette.rb
+++ app/components/main_page/icon_roulette.rb
@@ -2,7 +2,7 @@
 
 module MainPage
   class IconRoulette < ViewComponent::Base
-    POSSIBLE_ICONS = %w[lemon poo bomb hippo umbrella tag barcode book droplet money-bill paint-roller gamepad palette poo-storm hand-point-up shower otter kiwi-bird worm wave-square smoking shrimp pepper-hot florin-sign dog chess-king chess-rook chess-queen chess-pawn chess-knight chess-bishop cat burger bottle-water reddit pix minbit markdown gulp bed bacterium bacon angles-right angle-right computer-mouse computer compact-disc graduation-cap frog].freeze # rubocop:disable Layout/LineLength
+    POSSIBLE_ICONS = %w[lemon poo bomb hippo umbrella tag barcode book droplet money-bill paint-roller gamepad palette poo-storm hand-point-up shower otter kiwi-bird worm wave-square smoking shrimp pepper-hot florin-sign dog chess-king chess-rook chess-queen chess-pawn chess-knight chess-bishop cat burger bottle-water bed bacterium bacon angles-right angle-right computer-mouse computer compact-disc graduation-cap frog].freeze # rubocop:disable Layout/LineLength
     POSSIBLE_COLORS = %w[slate-600 zinc-700 stone-500 red-400 orange-400 yellow-700 lime-500 green-400 teal-400 blue-500 indigo-800 violet-700 purple-400 pink-600 rose-400].freeze # rubocop:disable Layout/LineLength
 
     def icons_data

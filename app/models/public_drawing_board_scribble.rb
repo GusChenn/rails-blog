7bc2fb33fa597b45cd8c4bd319028a775406ec77
Gustavo Chen <gustavo.sao@gmail.com>
2025-01-26 14:15:46 -0300

    Create method to fetch scribble encoded image data

--- app/models/public_drawing_board_scribble.rb
+++ app/models/public_drawing_board_scribble.rb
@@ -1,5 +1,5 @@
-# == Schema Information
-#
+# frozen_string_literal: true
+
 # Table name: public_drawing_board_scribbles
 #
 #  id            :bigint           not null, primary key
@@ -11,4 +11,8 @@
 # I plan to make the scribbles 1000x300 pixels for now
 class PublicDrawingBoardScribble < ApplicationRecord
   validates :scribble_data, presence: true
+
+  def scribble_base64_encoded_data
+    scribble_data.split(',')[1]
+  end
 end

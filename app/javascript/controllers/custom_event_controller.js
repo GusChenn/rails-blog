fe838e83ac09b69fa35d7ba8f3e529116e89f3b2
Gustavo Chen <gustavo.sao@gmail.com>
2025-07-24 19:47:38 -0300

    Progress checkpoint

--- app/javascript/controllers/custom_event_controller.js
+++ app/javascript/controllers/custom_event_controller.js
@@ -1,28 +1,88 @@
 import { Controller } from "@hotwired/stimulus"
 
 class EventBus {
-  /* 
-  Event: User action and its callback
-  Subscription: Explicit declaration that an element is listening for a specific event
-  */
-
   constructor() {
     this.events = {}
   }
 
-  setEvent(eventName, callback) {
-    this.events[eventName] = {
+  broadcast(eventName) {
+    const subscribedElements = this.events[eventName]?.subscribedElements
+
+    if(!subscribedElements) return
+
+    subscribedElements.forEach(subscription => {
+      subscription.callback.call(subscription.element)
+    })
+  }
+
+  subscribeElement(element, eventName, callback) {
+    if(!this.events[eventName]) {
+      this._createEvent(eventName)
+    }
+
+    this.events[eventName].subscribedElements.push({
+      element,
       callback
+    })
+  }
+
+  _createEvent(eventName) {
+    this.events[eventName] = {
+      subscribedElements: [],
     }
   }
+}
+
+class EventBusElement {
+  constructor({ eventBus, data }) {
+    this.eventBus = eventBus
+    this.data = data
+  }
+
+  subscribe(eventName, callback) {
+    this.eventBus.subscribeElement(this, eventName, callback)
+  }
+
+  // notifyEventBus(eventName) {
+  //   this.eventBus.broadcast(eventName)
+  // }
 }
 
 export default class extends Controller {
   static targets = []
 
   connect() {
-  }
+    // 1. Create the event bus and its elements
+    const eventBus = new EventBus()
+    const eventBusElement1 = new EventBusElement({
+      eventBus,
+      data: {
+        name: 'first element'
+      }
+    })
+    const eventBusElement2 = new EventBusElement({
+      eventBus,
+      data: {
+        name: 'second element'
+      }
+    })
 
-  disconnect() {
+    // 3. Subscribe the elements to the event bus
+    eventBusElement1.subscribe(
+      'hello',
+      function() {
+        console.log(`Hello from ${this.data.name}`)
+      }
+    )
+
+    eventBusElement2.subscribe(
+      'goodbye',
+      function() {
+        console.log(`Goodbye from ${this.data.name}`)
+      }
+    )
+
+    // 3. Make the event bus trigger the callback
+    eventBus.broadcast('goodbye')
   }
 }
